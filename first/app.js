f06cc8ba71839fc5a20a2f25544b142ae2f49131
shubham singh <singhshubham68738@gmail.com>
2025-04-04 01:44:15 +0530

    first

--- first/app.js
+++ first/app.js
@@ -1,27 +1,7 @@
 // core Modules:- HTMLOutputElement,https,fs,path,Os
 const http=require('http');
-const fs=require('fs');
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url=='/'){
-        res.write('<html><head><title></title></head><body><form action="/message" method="POST"><input></input><button type="submit">send</button></form></body></html>');
-        return res.end();
-    }
-    if(url=='/message' &&method=="POST"){
-        fs.writeFileSync('message.txt',"dummy");
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body>');
-    res.write('<h1>Hello from my Node.js Server!</h1>');
-    res.write('</body>');
-    res.end();
-    console.log(req);
-});
 
+const routes=require('./routes')
+const server=http.createServer(routes.handler);
+console.log(routes.message);
 server.listen(3000);
