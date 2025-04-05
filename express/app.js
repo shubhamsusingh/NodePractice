526829cff926856975ad13bf92ce438650037284
shubham singh <singhshubham68738@gmail.com>
2025-04-06 01:42:30 +0530

    working

--- express/app.js
+++ express/app.js
@@ -1,27 +1,24 @@
 const express=require('express');
 const bodyparse=require('body-parser');
 
+
 const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRouts= require('./routes/shop');
+
+
 app.use(bodyparse.urlencoded({extended:false}));
-app.use('/',(req,res,next)=>{
+// app.use('/',(req,res,next)=>{
    
-    next(); // Allows the request to continue to the next middleware in line
-});
-app.use('/add-product',(req,res,next)=>{
-   
-    res.send('<h1>Add product </h1><form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Submit</button></form>')
-    
+//     next(); // Allows the request to continue to the next middleware in line
+// });
+app.use(adminRoutes);
+app.use(shopRouts);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.use('/product',(req,res,next)=>{
-    const data=req.body;
-    console.log(data);
-   res.redirect("/");
-    
-})
-app.use('/',(req,res,next)=>{
-    
-    res.send('<h1>Helllo response from node</h1>')
-    
-})
+
 app.listen(3000);
 
---
+++ express/routes/admin.js
@@ -0,0 +1,17 @@
+const path=require('path');
+const express=require('express');
+
+const router=express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+   
+    // res.send('<h1>Add product </h1><form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Submit</button></form>')
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+})
+router.post('/product',(req,res,next)=>{
+    const data=req.body;
+    console.log(data);
+   res.redirect("/");
+    
+})
+module.exports=router;
