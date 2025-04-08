7c9cda5af999712d54ba0991518be3574926d95b
shubham Singh <singhshubham68738@gmail.com>
2025-04-08 14:41:35 +0530

    working

--- express/app.js
+++ express/app.js
@@ -1,14 +1,18 @@
 const express=require('express');
 const bodyparse=require('body-parser');
+const pathImp=require('path');
+
 
 
 const app=express();
 
 const adminRoutes=require('./routes/admin');
 const shopRouts= require('./routes/shop');
+const path = require('./utils/path');
 
 
 app.use(bodyparse.urlencoded({extended:false}));
+app.use(express.static(pathImp.join(__dirname,'public')));
 // app.use('/',(req,res,next)=>{
    
 //     next(); // Allows the request to continue to the next middleware in line
