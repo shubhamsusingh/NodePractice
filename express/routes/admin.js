8416810527868bbd502dc8c2870b5db8d2ef2871
shubham Singh <singhshubham68738@gmail.com>
2025-04-09 13:29:19 +0530

    handlebars

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -9,13 +9,13 @@ router.get('/add-product',(req,res,next)=>{
    
     // res.send('<h1>Add product </h1><form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Submit</button></form>')
     // res.sendFile(path.join(rootDir,'views','add-product.html'));
-    res.render("add-product",{pageTitle:"Add-Product",path:"admin/add-product"})
+    res.render("add-product",{pageTitle:"Add-Product",path:"admin/add-product",activeProduct:true,formCSS:true,productCSS:true})
 })
 router.post('/product',(req,res,next)=>{
     // const data=req.body;
     // console.log(data);
     products.push({title:req.body.title});
-   res.redirect("/",{pageTitle:"Shop"});
+   res.redirect("/");
     
 })
 // module.exports=router;
