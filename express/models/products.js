0e2214813107fca68347292df36971b8d20f1f21
shubham Singh <singhshubham68738@gmail.com>
2025-04-11 17:29:43 +0530

    working

--- express/models/products.js
+++ express/models/products.js
@@ -1,50 +1,40 @@
 const fs= require('fs');
 const path=require('path');
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
 
+const getProductsFromFile = cb=>{
+    
+    fs.readFile(p,(err,filrContent)=>{
+        if(err){
+          return cb([]);
+        }else{
+
+            cb(JSON.parse(filrContent)) ;
+        }
+    })
+}
 module.exports = class Product{
     constructor(t){
         this.title=t;
     }
 
     save() {
-        const p = path.join(
-          path.dirname(process.mainModule.filename),
-          'data',
-          'products.json'
-        );
-      
-        fs.readFile(p, (err, fileContent) => {
-          let products = [];
-          if (!err) {
-            try {
-              products = JSON.parse(fileContent);
-            } catch (parseErr) {
-              console.log('Error parsing JSON:', parseErr);
-              products = [];
-            }
+       getProductsFromFile(products=>{
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          if (err) {
+            console.log('Error writing file:', err);
           }
-          products.push(this);
-          fs.writeFile(p, JSON.stringify(products), (err) => {
-            if (err) {
-              console.log('Error writing file:', err);
-            }
-          });
         });
+       });
       }
       
 
     static fetchAll(cb){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-          );
-        fs.readFile(p,(err,filrContent)=>{
-            if(err){
-               cb([]);
-            }
-            cb(JSON.parse(filrContent)) ;
-        })
-        // return products;
+       getProductsFromFile(cb);
     }
 }
