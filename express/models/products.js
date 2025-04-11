680e08ebbaaa5bbb7abc3bbdd4dd2e8830e654e0
shubham Singh <singhshubham68738@gmail.com>
2025-04-11 16:56:26 +0530

    working

--- express/models/products.js
+++ express/models/products.js
@@ -1,14 +1,50 @@
-const products=[];
+const fs= require('fs');
+const path=require('path');
+
 module.exports = class Product{
     constructor(t){
         this.title=t;
     }
 
-    save(){
-        products.push(this);
-    }
+    save() {
+        const p = path.join(
+          path.dirname(process.mainModule.filename),
+          'data',
+          'products.json'
+        );
+      
+        fs.readFile(p, (err, fileContent) => {
+          let products = [];
+          if (!err) {
+            try {
+              products = JSON.parse(fileContent);
+            } catch (parseErr) {
+              console.log('Error parsing JSON:', parseErr);
+              products = [];
+            }
+          }
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), (err) => {
+            if (err) {
+              console.log('Error writing file:', err);
+            }
+          });
+        });
+      }
+      
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+          );
+        fs.readFile(p,(err,filrContent)=>{
+            if(err){
+               cb([]);
+            }
+            cb(JSON.parse(filrContent)) ;
+        })
+        // return products;
     }
 }
