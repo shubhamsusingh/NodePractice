d593868b400b02a587fd08bc94cc6e9b46ae218f
shubham Singh <singhshubham68738@gmail.com>
2025-04-11 19:13:57 +0530

    working

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
 
-const productController=require('../controllers/products');
+const adminController=require('../controllers/admin');
 
 const router = express.Router();
 
 
-router.get('/add-product',productController.getAddProduct);
-router.post('/product', productController.postAddProduct);
+router.get('/add-product',adminController.getAddProduct);
+// router.get('/products');
+router.post('/product', adminController.postAddProduct);
 // module.exports=router;
 // exports.routes = router;
 // exports.products = products;
