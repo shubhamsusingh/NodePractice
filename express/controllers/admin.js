139f2674c40817622a5da05ed037362d079e34a1
shubham singh <singhshubham68738@gmail.com>
2025-05-18 23:02:54 +0530

    working

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -13,8 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
+  // const product = new Product(null, title, imageUrl, description, price);
+  // product.save();
+  Product.create({
+    title:title,
+    price:price,
+    imageUrl:imageUrl,
+    description:description
+  }).then(result=>{
+    console.log("Data inserted succesfully");
+  }).catch(err=>{
+    console.log(err);
+  })
   res.redirect('/');
 };
 
