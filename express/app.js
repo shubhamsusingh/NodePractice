03edc1b647b0d436e5ec0d1ecba5c9dbe7c9530c
shubham singh <singhshubham68738@gmail.com>
2025-06-27 23:47:12 +0530

    working session

--- express/app.js
+++ express/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session=require('express-session');
 
 const errorController = require('./controllers/error');
 const sequelize= require('./util/database');
@@ -11,6 +12,7 @@ const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -29,6 +31,7 @@ const { name } = require('ejs');
 // });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'my secret',resave:false,saveUninitialized:false}));
 app.use((req,res,next)=>{
     User.findByPk(1)
     .then(user=>{
