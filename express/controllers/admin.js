583eb8bbb8f4738f9fd2d485fa74532b1034c70e
shubham singh <singhshubham68738@gmail.com>
2025-08-04 18:03:05 +0530

    working on multer

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -15,12 +15,12 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.image;
+  const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
   // const product = new Product(null, title, imageUrl, description, price);
   // product.save();
-  console.log("image : "+imageUrl);
+  console.log("image : ",imageUrl);
   const errors=validationResult(req);
   if(!errors.isEmpty()){
     return res.status(422).render('admin/edit-product', {
