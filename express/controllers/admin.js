342afd8708c8c743727c55a3c89ba75be1550c0d
shubham singh <singhshubham68738@gmail.com>
2025-08-07 15:06:22 +0530

    working on file handle

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -15,12 +15,27 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
   // const product = new Product(null, title, imageUrl, description, price);
   // product.save();
-  console.log("image : ",imageUrl);
+  // console.log("image : ",imageUrl);
+  if(!image){
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      product: {
+        title:title,
+        price:price,
+        description:description,
+      },
+       hasError:true,
+       errorMessage:"Attach File is not an image.",
+       validationErrors:[]
+    });
+  }
   const errors=validationResult(req);
   if(!errors.isEmpty()){
     return res.status(422).render('admin/edit-product', {
@@ -29,7 +44,7 @@ exports.postAddProduct = (req, res, next) => {
       editing: false,
       product: {
         title:title,
-        imageUrl:imageUrl,
+        imageUrl:image,
         price:price,
         description:description,
       },
@@ -38,6 +53,7 @@ exports.postAddProduct = (req, res, next) => {
        validationErrors:errors.array()
     });
   }
+  const imageUrl=image.path;
   req.user.createProduct({
   title: title,
   price: price,
@@ -96,7 +112,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
   Product.findByPk(prodId)
   .then(product=>{
@@ -105,22 +121,20 @@ exports.postEditProduct = (req, res, next) => {
     }
     product.title=updatedTitle;
     product.price=updatedPrice;
-    product.imageUrl=updatedImageUrl;
+    if(image){
+      product.imageUrl=image.path;
+    }
     product.description=updatedDesc;
    return product.save().then(result=>{
     console.log('UPDATED PRODUCT!');
     res.redirect('/admin/products');
   })
   })
-  
   .catch(err=>{
   const error=new Error(err);
   error.httpStatusCode=500;
   return next(error);
-  })
-    
-  
-  
+  }) 
 };
 
 exports.getProducts = (req, res, next) => {
